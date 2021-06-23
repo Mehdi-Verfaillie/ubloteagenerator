c537a6b6ff231657709c1d9cff759f6a975e0049
Mehdi <mehdi.verfaillie@hetic.net>
2021-06-23 16:33:11 +0200

    menu component wip & upgrade packages

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,10 +1,49 @@
 import React from 'react';
 import {
-    MenuContainer
+    MenuContainer,
+    MenuTitle,
+    CategoryContainer,
+    CategoryTitle,
+    CategoryTextInput,
+    MenuColorInput,
 } from './style';
+import Slider from 'rc-slider';
+import 'rc-slider/assets/index.css';
+
 
 export default function Menu() {
     return (
-        <MenuContainer />
+        <MenuContainer>
+            <MenuTitle>BUBBLE TEA GENERATOR</MenuTitle>
+
+            <CategoryContainer>
+                <CategoryTitle>NOM</CategoryTitle>
+                <CategoryTextInput
+                    placeholder="Hello"
+                />
+            </CategoryContainer>
+
+            {['GOBELET', 'TEA', 'TAPIOCA'].map((element, index) => (
+                <CategoryContainer key={index}>
+                    <CategoryTitle>{element}</CategoryTitle>
+                    <MenuColorInput />
+                </CategoryContainer>
+            ))}
+
+            <CategoryContainer>
+                <CategoryTitle>TAILLE</CategoryTitle>
+                <Slider
+                    defaultValue={0}
+                    // value={0}
+                    startPoint={0}
+                    min={0}
+                    max={10}
+                    onChange={0}
+                    railStyle={''}
+                    dotStyle={''}
+                />
+            </CategoryContainer>
+
+        </MenuContainer>
     );
 }
--- src/components/Menu/style.js
+++ src/components/Menu/style.js
@@ -1,9 +1,55 @@
 import styled from 'styled-components';
 
 const MenuContainer = styled.div`
-    width: 200px;
+    width: 22%;
+    min-width: 250px;
     height: 100vh;
     background-color: red;
+    padding: 30px 5px;
 `;
 
-export { MenuContainer };
+const MenuTitle = styled.p`
+    color: #FFF;
+    font-size: 18;
+    font-family: Arial;
+    text-transform: uppercase;
+    text-align: center;
+    margin-bottom: 50px;
+`;
+
+const CategoryContainer = styled.div`
+    display: flex;
+    background-color: purple;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0 10px;
+    margin-bottom: 10px;
+`;
+
+const CategoryTitle = styled.p`
+    color: #FFF;
+    font-size: 18;
+    font-family: Arial;
+    text-transform: uppercase;
+`;
+
+const CategoryTextInput = styled.input`
+    width: 100px;
+    height: 40px;
+    background-color: #FFF;
+`;
+
+const MenuColorInput = styled.div`
+    width: 30px;
+    height: 30px;
+    background-color: yellow;
+`;
+
+export {
+    MenuContainer,
+    MenuTitle,
+    CategoryContainer,
+    CategoryTitle,
+    CategoryTextInput,
+    MenuColorInput,
+};
